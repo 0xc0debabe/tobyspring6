50cc3975d7e621144c47b9aaa903988d33ec9f15
0xc0debabe <kongminoo@naver.com>
2024-09-24 13:08:30 +0900

    [9/24 토비의 스프링6 강의 - Section4]
    스프링을 통한 테스트

--- src/test/java/tobyspring/hellospring/SortTest.java
+++ src/test/java/tobyspring/hellospring/SortTest.java
@@ -5,10 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class SortTest {
 
@@ -39,4 +36,5 @@ class SortTest {
 
         Assertions.assertThat(list).isEqualTo(List.of("b", "aa", "ccc"));
     }
+
 }
